9015c6d08f3cfce2a04a54e76cb213bba3e29ee2
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2022-07-13 16:29:41 +0300

    Create OpacityView.swift

---
+++ OpacityView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct TestView: View {
+    
+    @State private var opacity : Double = 1.0
+    
+    var body: some View {
+        VStack {
+            Image("horizontal_gozen_pass_logo")
+                .frame(width: 200,
+                        height: 200,
+                        alignment: .center)
+                .opacity(opacity)
+                
+            Button {
+                withAnimation(.easeInOut(duration: 1.0)){
+                    if opacity == 0.0 {
+                        opacity = 1.0
+                    } else if opacity == 1.0 {
+                        opacity = 0.0
+                    }
+                }
+            } label: {
+                Text("START ANIMATE")
+                    .padding(.all,10)
+                    .foregroundColor(Color.black)
+                    .background {
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.black)
+                    }
+            }
+        }.frame(width: UIScreen.main.bounds.width,
+                height: UIScreen.main.bounds.height,
+                alignment: .center)
+        .background(Color.green)
+    }
+}
